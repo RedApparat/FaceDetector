02141c9b7447531ee6e9cbe03f666ea7e948c90e
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-07-04 10:10:22 +0200

    Added Android wrapper skeleton

---
+++ Android/app/src/main/java/io/fotoapparat/facedetector/app/MainActivity.java
@@ -0,0 +1,14 @@
+package io.fotoapparat.facedetector.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(io.fotoapparat.facedetector.R.layout.activity_main);
+    }
+
+}
