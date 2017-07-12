ffbb73e994d3891c3357252be38505235f7eeb6f
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-07-12 17:34:10 +0200

    Added demo app

---
+++ Example/Android/FaceDetectorExample/app/src/main/java/io/fotoapparat/facedetector/example/MainActivity.java
@@ -0,0 +1,131 @@
+package io.fotoapparat.facedetector.example;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
+
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.FotoapparatSwitcher;
+import io.fotoapparat.facedetector.Rectangle;
+import io.fotoapparat.facedetector.processor.FaceDetectorProcessor;
+import io.fotoapparat.facedetector.view.RectanglesView;
+import io.fotoapparat.parameter.LensPosition;
+import io.fotoapparat.view.CameraView;
+
+import static io.fotoapparat.log.Loggers.fileLogger;
+import static io.fotoapparat.log.Loggers.logcat;
+import static io.fotoapparat.log.Loggers.loggers;
+import static io.fotoapparat.parameter.selector.LensPositionSelectors.lensPosition;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final PermissionsDelegate permissionsDelegate = new PermissionsDelegate(this);
+    private boolean hasCameraPermission;
+    private CameraView cameraView;
+    private RectanglesView rectanglesView;
+
+    private FotoapparatSwitcher fotoapparatSwitcher;
+    private Fotoapparat frontFotoapparat;
+    private Fotoapparat backFotoapparat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (CameraView) findViewById(R.id.camera_view);
+        rectanglesView = (RectanglesView) findViewById(R.id.rectanglesView);
+        hasCameraPermission = permissionsDelegate.hasCameraPermission();
+
+        if (hasCameraPermission) {
+            cameraView.setVisibility(View.VISIBLE);
+        } else {
+            permissionsDelegate.requestCameraPermission();
+        }
+
+        frontFotoapparat = createFotoapparat(LensPosition.FRONT);
+        backFotoapparat = createFotoapparat(LensPosition.BACK);
+        fotoapparatSwitcher = FotoapparatSwitcher.withDefault(backFotoapparat);
+
+        View switchCameraButton = findViewById(R.id.switchCamera);
+        switchCameraButton.setVisibility(
+                canSwitchCameras()
+                        ? View.VISIBLE
+                        : View.GONE
+        );
+        switchCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchCamera();
+            }
+        });
+    }
+
+    private boolean canSwitchCameras() {
+        return frontFotoapparat.isAvailable() == backFotoapparat.isAvailable();
+    }
+
+    private Fotoapparat createFotoapparat(LensPosition position) {
+        return Fotoapparat
+                .with(this)
+                .into(cameraView)
+                .lensPosition(lensPosition(position))
+                .frameProcessor(
+                        FaceDetectorProcessor.with(this)
+                                .listener(new FaceDetectorProcessor.OnFacesDetectedListener() {
+                                    @Override
+                                    public void onFacesDetected(List<Rectangle> faces) {
+                                        Log.d("&&&", "Detected faces: " + faces.size());
+
+                                        rectanglesView.setRectangles(faces);
+                                    }
+                                })
+                                .build()
+                )
+                .logger(loggers(
+                        logcat(),
+                        fileLogger(this)
+                ))
+                .build();
+    }
+
+    private void switchCamera() {
+        if (fotoapparatSwitcher.getCurrentFotoapparat() == frontFotoapparat) {
+            fotoapparatSwitcher.switchTo(backFotoapparat);
+        } else {
+            fotoapparatSwitcher.switchTo(frontFotoapparat);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (hasCameraPermission) {
+            fotoapparatSwitcher.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (hasCameraPermission) {
+            fotoapparatSwitcher.stop();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (permissionsDelegate.resultGranted(requestCode, permissions, grantResults)) {
+            fotoapparatSwitcher.start();
+            cameraView.setVisibility(View.VISIBLE);
+        }
+    }
+
+}
---
+++ Example/Android/FaceDetectorExample/app/src/main/java/io/fotoapparat/facedetector/example/PermissionsDelegate.java
@@ -0,0 +1,61 @@
+package io.fotoapparat.facedetector.example;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+class PermissionsDelegate {
+
+    private static final int REQUEST_CODE = 10;
+    private final Activity activity;
+
+    PermissionsDelegate(Activity activity) {
+        this.activity = activity;
+    }
+
+    boolean hasCameraPermission() {
+        int permissionCheckResult = ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.CAMERA
+        );
+        return permissionCheckResult == PackageManager.PERMISSION_GRANTED;
+    }
+
+    void requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+                activity,
+                new String[]{Manifest.permission.CAMERA},
+                REQUEST_CODE
+        );
+    }
+
+    boolean resultGranted(int requestCode,
+                          String[] permissions,
+                          int[] grantResults) {
+
+        if (requestCode != REQUEST_CODE) {
+            return false;
+        }
+
+        if (grantResults.length < 1) {
+            return false;
+        }
+        if (!(permissions[0].equals(Manifest.permission.CAMERA))) {
+            return false;
+        }
+
+        View noPermissionView = activity.findViewById(R.id.no_permission);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            noPermissionView.setVisibility(View.GONE);
+            return true;
+        }
+
+        requestCameraPermission();
+        noPermissionView.setVisibility(View.VISIBLE);
+        return false;
+    }
+}
